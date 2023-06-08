64a12b7c686ddc600dd5cec800a00e75b6850bf7
codebyharshit <harshitagrawal048@gmail.com>
2023-06-08 17:17:40 +0530

    forntend code updated with logic update

--- src/ABI/Token.js
+++ src/ABI/Token.js
@@ -1,9 +1,5 @@
 const Token = [
-  {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
   {
     anonymous: false,
     inputs: [
@@ -75,91 +71,58 @@ const Token = [
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
     ],
     name: "allowance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
     ],
     name: "approve",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
+    name: "burn",
+    outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address",
-      },
+      { internalType: "address", name: "account", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
     ],
-    name: "balanceOf",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
+    name: "burnFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [],
     name: "decimals",
-    outputs: [
-      {
-        internalType: "uint8",
-        name: "",
-        type: "uint8",
-      },
-    ],
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
+      { internalType: "address", name: "spender", type: "address" },
       {
         internalType: "uint256",
         name: "subtractedValue",
@@ -167,52 +130,24 @@ const Token = [
       },
     ],
     name: "decreaseAllowance",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "addedValue",
-        type: "uint256",
-      },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "addedValue", type: "uint256" },
     ],
     name: "increaseAllowance",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
     ],
     name: "mint",
     outputs: [],
@@ -222,26 +157,14 @@ const Token = [
   {
     inputs: [],
     name: "name",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
+    outputs: [{ internalType: "string", name: "", type: "string" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
     name: "owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
+    outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
     type: "function",
   },
@@ -255,90 +178,40 @@ const Token = [
   {
     inputs: [],
     name: "symbol",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
+    outputs: [{ internalType: "string", name: "", type: "string" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
     name: "totalSupply",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
     ],
     name: "transfer",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "from",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
     ],
     name: "transferFrom",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
     name: "transferOwnership",
     outputs: [],
     stateMutability: "nonpayable",
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import "./index.css";
 import Web3Modal from "web3modal";
-import { bridgeEth, bridgeMatic } from "./config";
+import { tokenEth, tokenMatic, bridgeEth, bridgeMatic, admin } from "./config";
 import { ethers } from "ethers";
 import { parseEther } from "ethers";
-import { JsonRpcProvider } from "ethers";
 import Web3 from "web3";
 import Token from "./ABI/Token";
 import Bridge from "./ABI/Bridge";
-// import CircularIndeterminate from "../Loader";
+import Swal from "sweetalert2";
 
 function App() {
   const [address, setAddress] = useState("");
@@ -33,50 +33,70 @@ function App() {
     setToChain(e.target.value);
   };
 
+  const getAllowance = async (from, TokenAddress, bridge) => {
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      await provider.send("eth_requestAccounts", [0]);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(TokenAddress, Token, provider);
+      const result = await contract.allowance(from, bridge);
+      const allowance = result.toString();
+      console.log(allowance);
+      return allowance;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     console.log(address, fromChain, toChain, amount);
 
     if (fromChain == "goerli") {
-      // const web3Modal = new Web3Modal();
-      // const connection = await web3Modal.connect();
-      // console.log("connection", connection);
-      // const provider = new ethers.BrowserProvider(connection);
-      // const signer = provider.getSigner();
-      // const contract = new ethers.Contract(bridgeEth, Bridge, signer);
-      // console.log(contract);
-      // const value = parseEther(amount); // Convert to token's decimal format if needed
-      // console.log("value", value.toString());
-      // const finalValue = value.toString();
-      // // const tx = await contract.toAdd();
-      // //   console.log("tx", tx);
-      // try {
-      //   const tx = await contract.bridgeToken(finalValue, "0");
-      //   await tx.wait();
-      //   console.log("Tokens transferred successfully!");
-      // } catch (error) {
-      //   console.error("Error transferring tokens:", error);
-      // }
       const web3Modal = new Web3Modal();
       const provider = await web3Modal.connect();
       const web3 = new Web3(provider);
       console.log("web3", web3);
       const accounts = await web3.eth.getAccounts();
       const contract = new web3.eth.Contract(Bridge, bridgeEth);
-      const value = parseEther(amount);
+      const value = parseEther(amount).toString();
       console.log("value", value.toString());
-      const finalValue = value.toString();
-      try {
-        setIsLoader(true);
-        const tx = await contract.methods
-          .bridgeToken(finalValue, 1)
-          .send({ from: accounts[0] });
-        setIsLoader(false);
-        console.log("Tokens transferred successfully!");
-      } catch (error) {
-        setIsLoader(true);
-        console.error("Error transferring tokens:", error);
+      const allowance = await getAllowance(accounts[0], tokenEth, bridgeEth);
+
+      if (allowance >= amount) {
+        try {
+          setIsLoader(true);
+          const tx = await contract.methods
+            .bridgeToken(value, 1)
+            .send({ from: accounts[0] });
+          setIsLoader(false);
+
+          Swal.fire({
+            title: "Transaction Successful!",
+            text: "Tokens transferred successfully!",
+            icon: "success",
+            confirmButtonText: "cancel",
+          });
+
+          console.log("Tokens transferred successfully!");
+        } catch (error) {
+          setIsLoader(true);
+          Swal.fire({
+            title: "Transaction Fail!",
+            text: "Tokens Transaction Fail!",
+            icon: "warning",
+            confirmButtonText: "cancel",
+          });
+          console.error("Error transferring tokens:", error);
+        }
+      } else {
+        Swal.fire({
+          title: "Transaction Fail!",
+          text: "Please allow enough tokens before proceeding!",
+          icon: "warning",
+          confirmButtonText: "cancel",
+        });
       }
     } else {
       const web3Modal = new Web3Modal();
@@ -85,57 +105,108 @@ function App() {
       console.log("web3", web3);
       const accounts = await web3.eth.getAccounts();
       const contract = new web3.eth.Contract(Bridge, bridgeMatic);
-      const value = parseEther(amount);
-      console.log("value", value.toString());
-      const finalValue = value.toString();
-      try {
-        setIsLoader(true);
-        const tx = await contract.methods
-          .bridgeToken(finalValue, 1)
-          .send({ from: accounts[0] });
-        setIsLoader(false);
-        console.log("Tokens transferred successfully!");
-      } catch (error) {
-        setIsLoader(false);
-        console.error("Error transferring tokens:", error);
+      const value = parseEther(amount).toString();
+      console.log("value", value);
+      const allowance = await getAllowance(
+        accounts[0],
+        tokenMatic,
+        bridgeMatic
+      );
+
+      if (allowance >= amount) {
+        try {
+          setIsLoader(true);
+          const tx = await contract.methods
+            .bridgeToken(value, 1)
+            .send({ from: accounts[0] });
+          setIsLoader(false);
+          Swal.fire({
+            title: "Transaction Successful!",
+            text: "Tokens transferred successfully!",
+            icon: "success",
+            confirmButtonText: "cancel",
+          });
+          console.log("Tokens transferred successfully!");
+        } catch (error) {
+          setIsLoader(false);
+          Swal.fire({
+            title: "Transaction Fail!",
+            text: "Tokens Transaction Fail!",
+            icon: "warning",
+            confirmButtonText: "cancel",
+          });
+          console.error("Error transferring tokens:", error);
+        }
+      } else {
+        Swal.fire({
+          title: "Transaction Fail!",
+          text: "Please allow enough tokens before proceeding!",
+          icon: "warning",
+          confirmButtonText: "cancel",
+        });
       }
     }
   };
 
   return (
-    <div className="App">
-      {isLoader ? <h1>Loading...</h1> : ""}
-      <h1>Bridge Application</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter Address:
-          <input type="text" value={address} onChange={handleAddressChange} />
+    <div className="App bg-gray-900 text-white min-h-screen flex flex-col justify-center items-center">
+      {isLoader ? <h1 className="text-4xl mb-6 text-white">Loading...</h1> : ""}
+      <h1 className="text-4xl mb-6 text-white ">Bridge Application</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <label className="flex flex-col">
+          <span className="mb-1">Enter Address:</span>
+          <input
+            type="text"
+            value={address}
+            onChange={handleAddressChange}
+            className="rounded bg-gray-800 text-white px-4 py-2"
+          />
         </label>
-        <br />
-        <label>
-          <label>
-            Enter Amount:
-            <input type="number" value={amount} onChange={handleAmountChange} />
+        <div className="flex flex-col gap-2">
+          <label className="flex flex-col">
+            <span className="mb-1">Enter Amount:</span>
+            <input
+              type="number"
+              value={amount}
+              onChange={handleAmountChange}
+              className="rounded bg-gray-800 text-white px-4 py-2"
+            />
           </label>
-          Choose Chain to Bridge From:
-          <select value={fromChain} onChange={handleFromChainChange}>
-            <option value="">Select a chain</option>
-            <option value="goerli">Ethreum Goerli</option>
-            <option value="matic">Polygon Matic</option>
-          </select>
-        </label>
-        <br />
-        <br />
-        <label>
-          Choose Chain to Mint Tokens To:
-          <select value={toChain} onChange={handleToChainChange}>
-            <option value="">Select a chain</option>
-            <option value="goerli">Ethreum Goerli</option>
-            <option value="matic">Polygon Matic</option>
-          </select>
-        </label>
-        <br />
-        <button type="submit">Bridge</button>
+          <label className="flex flex-col">
+            <span className="mb-1">Choose Chain to Bridge From:</span>
+            <select
+              value={fromChain}
+              onChange={handleFromChainChange}
+              className="rounded bg-gray-800 text-white px-4 py-2"
+            >
+              <option value="">Select a chain</option>
+              <option value="goerli">Ethreum Goerli</option>
+              <option value="matic">Polygon Matic</option>
+            </select>
+          </label>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="flex flex-col">
+            <span className="mb-1">Choose Chain to Mint Tokens To:</span>
+            <select
+              value={toChain}
+              onChange={handleToChainChange}
+              className="rounded bg-gray-800 text-white px-4 py-2"
+            >
+              <option value="">Select a chain</option>
+              <option value="goerli">Ethreum Goerli</option>
+              <option value="matic">Polygon Matic</option>
+            </select>
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2"
+        >
+          Bridge
+        </button>
       </form>
     </div>
   );
